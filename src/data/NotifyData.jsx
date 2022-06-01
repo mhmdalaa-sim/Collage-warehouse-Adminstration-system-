82e492ed8983e7e086b5870a8c4681c45719fd9f
mhmdalaa-sim <80651664+mhmdalaa-sim@users.noreply.github.com>
2022-06-01 03:21:45 +0200

    Add files via upload

---
+++ src/data/NotifyData.jsx
@@ -0,0 +1,25 @@
+const NotifyData = [
+    {
+      id: 1,
+      content: "تم تحويل شاشه الي مخزن حاسب",
+      date: "1/1/1"
+    },
+    {
+       
+      id: 2,
+      content: "تم تحويل ماوس الي مخزن حاسب",
+     date: "2/1/1",
+    },
+    {   id: 3,
+        content: "تم تم استلام طلباتك بنجاح ",
+        date: "3/1/1",
+    },
+
+    {   id: 4,
+        content: "تم تحويل  طلباتك بنجاح ",
+      date: "3/1/1",
+  },
+  
+  ]
+  
+  export default NotifyData
