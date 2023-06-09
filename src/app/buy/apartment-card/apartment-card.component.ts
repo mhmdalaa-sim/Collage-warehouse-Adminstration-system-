e1048be4fbe7965112cd379a423b1a6f944efffc
Mohamed Alaa <80651664+mhmdalaa-sim@users.noreply.github.com>
2023-06-10 00:24:27 +0300

    Add files via upload

---
+++ src/app/buy/apartment-card/apartment-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apartment-card',
+  templateUrl: './apartment-card.component.html',
+  styleUrls: ['./apartment-card.component.css']
+})
+export class ApartmentCardComponent {
+
+}
---
+++ src/app/buy/buy.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ApartmentCardComponent } from './apartment-card/apartment-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainContentComponent,
+    ApartmentCardComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+
+  exports:[
+    MainContentComponent
+  ]
+})
+export class BuyModule { }
